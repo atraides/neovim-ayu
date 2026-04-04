12b1d4c42cdcfde129481c25a63334d572d06c8c
Dániel Hagyárossy <daniel@hagyarossy.hu>
2026-04-04 21:45:16 +0200

    fix: Restore lualine theme

--- lua/lualine/themes/ayu.lua
+++ lua/lualine/themes/ayu.lua
@@ -1,48 +1,36 @@
 local colors = require('ayu.colors')
-local M = {}
 
-M.mode_colors = function()
-  local mode_color = {
-    n = colors.bright_blue,
-    i = colors.green,
-    v = colors.magenta,
-    [' '] = colors.blue,
-    V = colors.magenta,
-    c = colors.warning,
-    no = colors.warning,
-    s = colors.opeator,
-    S = colors.operator,
-    ic = colors.yellow,
-    R = colors.green,
-    Rv = colors.green,
-    cv = colors.red,
-    ce = colors.red,
-    r = colors.cyan,
-    rm = colors.cyan,
-    ['r?'] = colors.cyan,
-    ['!'] = colors.red,
-    t = colors.red,
-  }
-  return { fg = mode_color[vim.fn.mode()] }
-end
-
-M.styles = {
-  branch = { fg = colors.green, gui = 'bold' },
-  filename = { fg = colors.magenta, gui = 'bold' },
-  diff = {
-    added = { fg = colors.vcs_added },
-    modified = { fg = colors.vcs_modified },
-    removed = { fg = colors.vcs_removed },
+local ayu = {
+  visual = {
+    a = { fg = colors.bg, bg = colors.accent, gui = 'bold' },
+    b = { fg = colors.accent, bg = colors.panel_bg },
   },
-  diagnostics = {
-    error = { fg = colors.error },
-    warn = { fg = colors.warning },
-    info = { fg = colors.cyan },
+  replace = {
+    a = { fg = colors.bg, bg = colors.markup, gui = 'bold' },
+    b = { fg = colors.markup, bg = colors.panel_bg },
   },
-  lsp_status = { fg = colors.black, gui = 'bold' },
-  location = { fg = colors.fg, gui = 'bold' },
-  progress = { fg = colors.fg, gui = 'bold' },
-  line_close = { fg = colors.blue, gui = 'bold' },
+  inactive = {
+    a = { fg = colors.fg, bg = colors.panel_bg, gui = 'bold' },
+    b = { fg = colors.fg, bg = colors.panel_bg },
+    c = { fg = colors.fg, bg = colors.panel_bg },
+  },
+  normal = {
+    a = { fg = colors.bg, bg = colors.entity, gui = 'bold' },
+    b = { fg = colors.entity, bg = colors.panel_bg },
+    c = { fg = colors.fg, bg = colors.panel_bg },
+  },
+  insert = {
+    a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
+    b = { fg = colors.string, bg = colors.panel_bg },
+  },
+  command = {
+    a = { fg = colors.bg, bg = colors.constant, gui = 'bold' },
+    b = { fg = colors.constant, bg = colors.panel_bg },
+  },
+  terminal = {
+    a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
+    b = { fg = colors.string, bg = colors.panel_bg },
+  },
 }
 
-return M
+return ayu
